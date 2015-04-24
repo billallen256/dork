e59976abdb5744d6a286cf57ec1b92a3d6ddeff8
Bill Allen <photo.allen@gmail.com>
2015-04-24 10:57:14 -0600

    Messing with Haskell a while ago.

--- haskell/ch01/WC.hs
+++ haskell/ch01/WC.hs
@@ -1,4 +1,5 @@
 -- file: ch01/WC.hs
 
 main = interact wordCount
-	where wordCount input = show (length (lines input)) ++ "\n"
+--	where wordCount input = show (length (lines input)) ++ "\n"
+	where wordCount input = show (sum [ length x | x <- (lines input) ]) ++ "\n"
