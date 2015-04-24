0c6172e32780587be8fc0054de18bc2baede716e
Bill Allen <photo.allen@gmail.com>
2015-04-24 11:19:10 -0600

    Resolved some issues with gulp and its deps, which are now in install.sh.

--- coffeescript/gulpfile.coffee
+++ coffeescript/gulpfile.coffee
@@ -1,9 +1,19 @@
+# based on http://pem-musing.blogspot.com/2014/02/a-gulp-of-coffee-your-gulpfile-in.html
+# and http://david.nowinsky.net/gulp-book/example/coffee.html
+
 gulp = require 'gulp'
+gutil = require 'gulp-util'
+coffee = require 'gulp-coffee'
 coffeelint = require 'gulp-coffeelint'
 
 gulp.task 'lint', ->
-    gulp.src './*.coffee'
+    gulp.src './test.coffee'
         .pipe coffeelint()
         .pipe coffeelint.reporter()
 
-gulp.task 'default', ['lint']
+gulp.task 'compile', ->
+    gulp.src './test.coffee'
+        .pipe coffee bare: true
+        .on 'error', gutil.log
+
+gulp.task 'default', ['lint', 'compile']
--- coffeescript/install.sh
+++ coffeescript/install.sh
@@ -3,16 +3,21 @@
 #sudo apt-get install -y coffeescript
 sudo apt-get install -y npm
 sudo apt-get install -y editorconfig
+mkdir -p ~/bin
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+
 npm install coffee-script
 ln -s ~/node_modules/.bin/coffee ~/bin/coffee
 ln -s ~/node_modules/.bin/cake ~/bin/cake
+
 npm install coffeelint
 ln -s ~/node_modules/.bin/coffeelint ~/bin/coffeelint
+
 npm install gulp
 ln -s ~/node_modules/.bin/gulp ~/bin/gulp
+npm install gulp-util
+npm install gulp-coffee
 npm install gulp-coffeelint
-sudo ln -s /usr/bin/nodejs /usr/bin/node
-mkdir -p ~/bin
 
 # generate coffeelint config with
 # coffeelint --makeconfig coffeelint.json
