2da43da7a46ca35224b3caedc9c12c7712f764ac
Bill Allen <photo.allen@gmail.com>
2015-05-07 16:01:23 -0600

    Messing with linear regression.

---
+++ go/linreg.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+func sum(a *[]float64) float64 {
+	s := 0.0
+
+	for _, x := range *a {
+		s += x
+	}
+
+	return s
+}
+
+func mean(a *[]float64) float64 {
+	return sum(a) / float64(len(*a))
+}
+
+func std(a *[]float64) float64 {
+	m := mean(a)
+	s := 0.0
+
+	for _, x := range *a {
+		s += math.Pow(x - m, 2.0)
+	}
+
+	return math.Sqrt(s / float64(len(*a)))
+}
+
+func readVals(f *os.File) (*[]float64, *[]float64) {
+	reader := csv.NewReader(f)
+	xs := make([]float64, 0, 10)
+	ys := make([]float64, 0, 10)
+
+	for {
+		r, err := reader.Read()
+
+		if err != nil {
+			break
+		}
+
+		x, xerr := strconv.ParseFloat(r[0], 64)
+		y, yerr := strconv.ParseFloat(r[1], 64)
+
+		if xerr != nil || yerr != nil {
+			continue
+		}
+
+		xs = append(xs, x)
+		ys = append(ys, y)
+	}
+
+	return &xs, &ys
+}
+
+func corr(X *[]float64, Y *[]float64) float64 {
+	xmean := mean(X)
+	ymean := mean(Y)
+	//x := make([]float64, len(*X))
+	//y := make([]float64, len(*Y))
+	xy := make([]float64, len(*X))
+	x2 := make([]float64, len(*X))
+	y2 := make([]float64, len(*Y))
+
+	for i := range *X {
+		x := (*X)[i] - xmean
+		y := (*Y)[i] - ymean
+		xy[i] = x * y
+		x2[i] = math.Pow(x, 2.0)
+		y2[i] = math.Pow(y, 2.0)
+	}
+
+	return sum(&xy) / math.Sqrt(sum(&x2) * sum(&y2))
+}
+
+func main() {
+	f, err := os.Open(os.Args[1])
+	defer f.Close()
+
+	if err != nil {
+		os.Exit(1)
+	}
+
+	xs, ys := readVals(f)
+
+	for i := range *xs {
+		fmt.Println((*xs)[i], (*ys)[i])
+	}
+
+	mx := mean(xs)
+	my := mean(ys)
+	sx := std(xs)
+	sy := std(ys)
+	r := corr(xs, ys)
+
+	slope := r * (sy / sx)
+	intercept := my - (slope * mx)
+
+	fmt.Println("mean x", mx, "mean y", my)
+	fmt.Println("std x", sx, "std y", sy)
+	fmt.Println("r", r)
+	fmt.Println("b", slope)
+	fmt.Println("A", intercept)
+}
